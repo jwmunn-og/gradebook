3ac241ad2d45883247312cd2c086363ec6b6d97d
jwmunn <contact@justinmunn.co>
2017-08-21 05:21:22 -0400

    List all courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#index"
+  resources :courses, only: [:index, :show]
   namespace :teacher do
     resources :courses
   end
